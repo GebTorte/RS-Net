3797513cdbda202a295e6292fa8648b20b8f334c
GeburtstagsTorte <simonsacher@gmail.com>
2024-05-02 17:35:16 +0200

    fixing hparam_opt_test script

--- srcv2_2/models/hparam_optimization_v2_testing.py
+++ srcv2_2/models/hparam_optimization_v2_testing.py
@@ -6,7 +6,7 @@ import numpy as np
 
 activation_functions = ['elu']
 loss_functions = ['categorical_crossentropy'] # ['binary_crossentropy']
-initializers = ['glorot_normal']
+initializers = ['glorot_normal', 'he_normal']
 learning_rates = [1e-5, 1e-6]
 use_batch_norm = ['True']
 l2regs = [1e-4, 1e-5]
@@ -16,7 +16,7 @@ epochs = [3, 5, 8]# [3, 10, 20, 40, 80, 160, 200, 200, 200, 200, 200, 200, 200, 
 dropout_on_last_layer_only=['True', 'False']
 last_layer_activation_func = 'softmax'
 satellite = "Landsat8"
-cls=[['clear', 'shadow', 'thin', 'cloud'], ['cloud', 'thin', 'clear', 'shadow'], ['clear', 'cloud', 'thin', 'shadow', 'snow', 'water'] ]
+cls_list=[['clear', 'cloud', 'thin', 'shadow', 'snow', 'water'] , ['clear', 'shadow', 'thin', 'cloud']]
 collapse_cls = 'False'
 
 # Train the models
@@ -30,7 +30,7 @@ for activation_func in activation_functions:
                             for epoch_no in epochs:                                
                                 for bands in band_combinations:
                                     for initializer in initializers:
-                                        for c in cls:
+                                        for cls in cls_list:
                                             # Hacky way to do to random search by overwriting actual values
                                             #learning_rate = int(np.random.uniform(10, 100, 1)[0]) * 1e-5  # Cast to int to avoid round() function (issues with floats)
                                             #l2reg = int(np.random.uniform(1, 1000, 1)[0]) * 1e-5  # 1 to 100 when using ground truth, 1 to 1000 when using fmask
@@ -67,6 +67,7 @@ for activation_func in activation_functions:
                                             print('epochs: ' + str(epoch_no))
                                             print('use_batch_norm: ' + str(use_bn))
                                             print('dropout_on_last_layer_only: ' + str(dropout_on_only_last_layer))
+                                            print('cls: ', str(cls))
                                             print('---------------------------------------------------')
                                             subprocess.check_call([interpreter,
                                                                 script,
